8cc4234685ae1ff8621cf8f409bcb504f5fde703
Rikki <rikki.walker@nhs.net>
2025-07-22 11:10:42 +0100

    Boyscouting. JIRA 2282

--- test/fhir/odt/organisation_test.rb
+++ test/fhir/odt/organisation_test.rb
@@ -7,8 +7,8 @@ module NdrLookup
       # The orgaisation class tests
       class OrganisationTest < Minitest::Test
         def test_organisation_should_have_model_like_behaviour
-          url  = ODT_ENDPOINT + '/Organization/X26'
-          file = File.new(RESPONSES_DIR + '/fhir/organisation_find_success_response.txt')
+          url  = "#{ODT_ENDPOINT}/Organization/X26"
+          file = File.new("#{RESPONSES_DIR}/fhir/organisation_find_success_response.txt")
           stub_request(:get, url).to_return(file)
           org = NdrLookup::Fhir::Odt::Organisation.find('X26')
 
@@ -16,8 +16,8 @@ module NdrLookup
         end
 
         def test_should_raise_error_if_not_valid_find_method
-          url  = ODT_ENDPOINT + '/Organization/all'
-          file = File.new(RESPONSES_DIR + '/fhir/organisation_all_not_acceptable_response.txt')
+          url  = "#{ODT_ENDPOINT}/Organization/all"
+          file = File.new("#{RESPONSES_DIR}/fhir/organisation_all_not_acceptable_response.txt")
           stub_request(:get, url).to_return(file)
           assert_raises do
             NdrLookup::Fhir::Odt::Organisation.all
