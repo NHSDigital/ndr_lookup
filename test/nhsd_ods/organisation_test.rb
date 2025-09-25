2a8fe09728fe3049cb22bdea307eede38312981d
Rikki <114223536+Rikki-Dev@users.noreply.github.com>
2025-09-25 10:16:02 +0100

    Fix Organisation.all test for lazy evaluation in ActiveResource (#19)
    * Fix Organisation.all test for lazy evaluation in ActiveResource
    
    * Rubocop on fix

--- test/nhsd_ods/organisation_test.rb
+++ test/nhsd_ods/organisation_test.rb
@@ -6,8 +6,8 @@ module NdrLookup
     # The orgaisation class tests
     class OrganisationTest < Minitest::Test
       def test_organisation_should_have_model_like_behaviour
-        url  = ODS_ENDPOINT + 'organisations/X26'
-        file = File.new(RESPONSES_DIR + '/nhsd_ods/organisation_find_success_response.txt')
+        url  = "#{ODS_ENDPOINT}organisations/X26"
+        file = File.new("#{RESPONSES_DIR}/nhsd_ods/organisation_find_success_response.txt")
         stub_request(:get, url).to_return(file)
         org = NdrLookup::NhsdOds::Organisation.find('X26')
 
@@ -15,12 +15,12 @@ module NdrLookup
       end
 
       def test_should_raise_error_if_not_valid_find_method
-        url  = ODS_ENDPOINT + 'organisations'
-        file = File.new(RESPONSES_DIR + '/nhsd_ods/organisation_all_not_acceptable_response.txt')
+        url  = "#{ODS_ENDPOINT}organisations"
+        file = File.new("#{RESPONSES_DIR}/nhsd_ods/organisation_all_not_acceptable_response.txt")
         stub_request(:get, url).to_return(file)
 
         assert_raises do
-          NdrLookup::NhsdOds::Organisation.all
+          NdrLookup::NhsdOds::Organisation.any?
         end
       end
     end
